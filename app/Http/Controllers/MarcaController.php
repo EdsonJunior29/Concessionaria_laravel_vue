ea42cb9726c672b825cae29df8a3f8e17952e719
Edson Junior <edsonjos61@gmail.com>
2022-05-23 13:38:05 -0300

    Validation of parameters when adding new brand.

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -21,6 +21,18 @@ class MarcaController extends Controller
 
     public function store(Request $request)
     {
+        $regras = [
+            'nome' => 'required|unique:marcas',
+            'imagem' => 'required'
+        ];
+
+        $feedback = [
+            'required' => 'O campo :atribute é obrigatório.',
+            'nome.unique' => 'O nome da marca já existe.'
+        ];
+
+        $request->validate($regras, $feedback);
+
         $marcas = $this->marca->create($request->all());
         return response()->json($marcas, 201);
     }
