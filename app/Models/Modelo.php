d56edaaf6d5ce9426429cccb44902b1eede09f13
Edson Junior <edsonjos61@gmail.com>
2022-05-25 09:05:08 -0300

    Implementing validation rules.

--- app/Models/Modelo.php
+++ app/Models/Modelo.php
@@ -9,6 +9,17 @@ class Modelo extends Model
 {
     use HasFactory;
 
+
+    protected $fillable = [
+        'marca_id',
+        'nome',
+        'imagem',
+        'numero_protas',
+        'lugares',
+        'air_bag',
+        'abs'
+    ];
+
     public function marca()
     {
         return $this->belongsTo(Marca::class);
@@ -17,5 +28,18 @@ class Modelo extends Model
     public function carros()
     {
         return $this->hasMany(Carro::class);
+    }
+
+    public function rules()
+    {
+        return [
+            'marca_id' => 'exists:marcas,id',
+            'nome' => 'required|unique:modelos,nome,'.$this->id.'|min:3',
+            'imagem' => 'required|file|mimes:png,jpeg,jpg',
+            'numero_protas' => 'required|integer|digits_between:1,5',
+            'lugares' => 'required|integer|digits_between:1,7',
+            'air_bag' => 'required|boolean',
+            'abs' => 'required|boolean'
+        ];
     }
 }
