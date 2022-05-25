55d610bd34328355efa814a0fb5831e7f3cb1d40
Edson Junior <edsonjos61@gmail.com>
2022-05-25 15:11:41 -0300

    Paasing attributes in URL to filter search.

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -13,9 +13,18 @@ class ModeloController extends Controller
         $this->modelo = $modelo;
     }
 
-    public function index()
+    public function index(Request $request)
     {
-        return response()->json($this->modelo->with('marca')->get(), 200);
+        $modelos = array();
+
+        if ($request->has('atributos')) {
+            $atributos = $request->atributos;
+            $modelos = $this->modelo->selectRaw($atributos)->get();
+        } else {
+            $modelos = $this->modelo->with('marca')->get();
+        }
+
+        return response()->json($modelos, 200);
     }
 
     public function store(Request $request)
