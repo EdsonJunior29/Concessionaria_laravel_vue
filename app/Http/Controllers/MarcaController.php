089a5c64feb87c9a3e7c65ad100c635dd59d195f
Edson Junior <edsonjos61@gmail.com>
2022-05-26 08:26:37 -0300

    Adding MarcaRepository to isolate the business rule.

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Marca;
+use App\Repositories\MarcaRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -16,32 +17,25 @@ class MarcaController extends Controller
 
     public function index(Request $request)
     {
-        $marcas = array();
+        $marcaRepository = new MarcaRepository($this->marca);
 
         if ($request->has('atributos_modelos')) {
-            $atributos_modelos = $request->atributos_modelos;
-            $marcas = $this->marca->with('modelos:id,'.$atributos_modelos);
+            $atributos_modelos ='modelos:id,'.$request->atributos_modelos;
+            $marcaRepository->selectAtributosRegistrosRelacionados($atributos_modelos);
         } else {
-            $marcas = $this->marca->with('modelos');
+            $marcaRepository->selectAtributosRegistrosRelacionados('modelos');
         }
 
         if ($request->has('filtro')) {
-            $filtros = explode(';', $request->filtro);
-
-            foreach ($filtros as $key => $condicoes) {
-                 $m = explode(':', $condicoes);
-                 $marcas = $marcas->where($m[0], $m[1], $m[2]);
-            }
+            $marcaRepository->filtro($request->filtro);
         }
 
         if ($request->has('atributos')) {
             $atributos = $request->atributos;
-            $marcas = $marcas->selectRaw($atributos)->get();
-        } else {
-            $marcas = $marcas->get();
+            $marcaRepository->selectAtributos($atributos);
         }
 
-        return response()->json($marcas, 200);
+        return response()->json($marcaRepository->getResultado(), 200);
     }
 
     public function store(Request $request)
---
+++ app/Repositories/MarcaRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MarcaRepository
+{
+    public function __construct($model)
+    {
+        $this->model = $model;
+    }
+
+    public function selectAtributosRegistrosRelacionados(string $atributos)
+    {
+        $this->model = $this->model->with($atributos);
+    }
+
+    public function filtro(string $filtros)
+    {
+        $filtros = explode(';', $filtros);
+        foreach ($filtros as $key => $condicoes) {
+            $m = explode(':', $condicoes);
+            $this->model = $this->model->where($m[0], $m[1], $m[2]);
+        }
+    }
+
+    public function selectAtributos(string $atributos)
+    {
+        $this->model = $this->model->selectRaw($atributos);
+    }
+
+    public function getResultado()
+    {
+        return $this->model->get();
+    }
+}
