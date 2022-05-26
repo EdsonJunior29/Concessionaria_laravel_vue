c6f3b5ec4037636dd336fb6a5c05c3af2271da57
Edson Junior <edsonjos61@gmail.com>
2022-05-26 09:13:45 -0300

    Creating an abstract repository.

--- app/Repositories/ModeloRepository.php
+++ app/Repositories/ModeloRepository.php
@@ -2,36 +2,6 @@
 
 namespace App\Repositories;
 
-use Illuminate\Database\Eloquent\Model;
-
-class ModeloRepository
+class ModeloRepository extends AbstractRepository
 {
-    public function __construct($model)
-    {
-        $this->model = $model;
-    }
-
-    public function selectAtributosRegistrosRelacionados(string $atributos)
-    {
-        $this->model = $this->model->with($atributos);
-    }
-
-    public function filtro(string $filtros)
-    {
-        $filtros = explode(';', $filtros);
-        foreach ($filtros as $key => $condicoes) {
-            $m = explode(':', $condicoes);
-            $this->model = $this->model->where($m[0], $m[1], $m[2]);
-        }
-    }
-
-    public function selectAtributos(string $atributos)
-    {
-        $this->model = $this->model->selectRaw($atributos);
-    }
-
-    public function getResultado()
-    {
-        return $this->model->get();
-    }
 }
