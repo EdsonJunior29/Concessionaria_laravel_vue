f828997f1a33651d54ac80526662056597188052
Edson Junior <edsonjos61@gmail.com>
2022-05-28 12:22:11 -0300

    Implementing the login method.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,9 +6,20 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login()
+    public function login(Request $request)
     {
-        return 'login....';
+        //Autenticação (email e senha)
+        $credenciais = $request->all(['email', 'password']);
+        $token = auth('api')->attempt($credenciais);
+
+        //retorna um JWT(Json Web Token)
+        if (!$token) {
+            //Usuário não autenticado.
+            //$token retornou false.
+            return response()->json(['errors' => 'Email ou Senha inválidos!'], 403);
+        }
+
+        return response()->json(['token' => $token], 200);
     }
 
     public function logout()
