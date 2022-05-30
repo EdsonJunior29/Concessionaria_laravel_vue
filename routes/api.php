8f245bcb31b1c4396d20ef68760a9a47a41fb609
Edson Junior <edsonjos61@gmail.com>
2022-05-30 17:44:24 -0300

    Implementing ME route to return authenticated user information.

--- routes/api.php
+++ routes/api.php
@@ -70,9 +70,10 @@ Route::prefix('v1') -> middleware('jwt.auth')-> group(function () {
         Route::patch('/{locacao}', [LocacaoController::class, 'update']);
         Route::delete('/{locacao}', [LocacaoController::class, 'destroy']);
     });
+
+    Route::post('me', [AuthController::class, 'me']);
 });
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('logout', [AuthController::class, 'logout']);
 Route::post('refresh', [AuthController::class, 'refresh']);
-Route::post('me', [AuthController::class, 'me']);
