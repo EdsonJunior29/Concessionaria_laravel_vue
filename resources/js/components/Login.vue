4c4c159576034840c23d26cce509c351c48d4e75
Edson Junior <edsonjos61@gmail.com>
2022-05-31 15:28:39 -0300

    Working propsto forward csrf token to frntEnd.

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -4,10 +4,9 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Login (Componente.vue)</div>
-
                 <div class="card-body">
                     <form method="POST" action="">
-
+                        <input type="hidden" name="_token" :value="csrf_token">
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">Email</label>
 
@@ -58,5 +57,8 @@
 </template>
 
 <script>
+export default{
+    props: ['csrf_token']
+}
 
 </script>
