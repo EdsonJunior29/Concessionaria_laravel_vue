ef726d3ea9e25eddb2e039f04ba4ce27799b0338
Edson Junior <edsonjos61@gmail.com>
2022-06-01 11:17:49 -0300

    Creating the table for tags.

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -2,7 +2,9 @@
     <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+
+            <!--Início do card de busca-->
+            <div class="card mb-5">
                 <div class="card-header">Busca de Marcas</div>
 
                 <div class="card-body">
@@ -35,6 +37,50 @@
                     <button type="submit" class="btn btn-primary btn-sm ">Pesquisar</button>
                 </div>
             </div>
+            <!--Fim do card de busca-->
+
+            <!--Início do card de Listagem de Marcas-->
+             <div class="card">
+                <div class="card-header">Listagem de Marcas</div>
+
+                <div class="card-body">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td colspan="2">Larry the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="card-footer">
+                    <button type="button" class="btn btn-primary btn-sm ">Adicionar</button>
+                </div>
+            </div>
+            <!--Fim do card de Listagem de Marcas-->
+
         </div>
     </div>
 </div>
