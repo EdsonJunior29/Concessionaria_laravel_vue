9efd0611976e061cbadecc6c8f79323f9316fe31
Edson Junior <edsonjos61@gmail.com>
2022-06-01 11:04:50 -0300

    Encapsulating input

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -3,10 +3,36 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Marcas</div>
+                <div class="card-header">Busca de Marcas</div>
 
                 <div class="card-body">
-                    <p> Manipulador de registro de Marcas </p>
+                    <div class="row">
+                        <div class="col mb-3">
+
+                            <input-container-component
+                            titulo="ID"
+                            id="inputId"
+                            id-help="idHelp"
+                            texto-help="Opcional. Informe o Id da marca">
+                                 <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
+                            </input-container-component>
+
+                        </div>
+                        <div class="col mb-3">
+
+                             <input-container-component
+                            titulo="Nome da marca"
+                            id="inputNome"
+                            nome-help="nomeHelp"
+                            texto-help="Opcional. Informe o Nome da Marca.">
+                                 <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome da marca">
+                            </input-container-component>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-primary btn-sm ">Pesquisar</button>
                 </div>
             </div>
         </div>
