b3d33ee4055af74547be30e9ad8b94679d198b67
Edson Junior <edsonjos61@gmail.com>
2022-06-01 13:34:32 -0300

    Creating modal

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -1,58 +1,91 @@
 <template>
     <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
 
-            <!--Início do card de busca-->
-            <card-component titulo="Busca de Marcas">
-                <template v-slot:conteudo>
-                    <div class="row">
-                        <div class="col mb-3">
+                <!--Início do card de busca-->
+                <card-component titulo="Busca de Marcas">
+                    <template v-slot:conteudo>
+                        <div class="row">
+                            <div class="col mb-3">
 
-                            <input-container-component
-                            titulo="ID"
-                            id="inputId"
-                            id-help="idHelp"
-                            texto-help="Opcional. Informe o Id da marca">
-                                 <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
-                            </input-container-component>
+                                <input-container-component
+                                titulo="ID"
+                                id="inputId"
+                                id-help="idHelp"
+                                texto-help="Opcional. Informe o Id da marca">
+                                    <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
+                                </input-container-component>
 
+                            </div>
+                            <div class="col mb-3">
+
+                                <input-container-component
+                                titulo="Nome da marca"
+                                id="inputNome"
+                                nome-help="nomeHelp"
+                                texto-help="Opcional. Informe o Nome da Marca.">
+                                    <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome da marca">
+                                </input-container-component>
+                            </div>
                         </div>
-                        <div class="col mb-3">
+                    </template>
 
-                             <input-container-component
-                            titulo="Nome da marca"
-                            id="inputNome"
-                            nome-help="nomeHelp"
-                            texto-help="Opcional. Informe o Nome da Marca.">
-                                 <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome da marca">
-                            </input-container-component>
-                        </div>
-                    </div>
-                </template>
+                    <template v-slot:rodape>
+                        <button type="submit" class="btn btn-primary btn-sm ">Pesquisar</button>
+                    </template>
+                </card-component>
 
-                <template v-slot:rodape>
-                     <button type="submit" class="btn btn-primary btn-sm ">Pesquisar</button>
-                </template>
-            </card-component>
+                <!--Fim do card de busca-->
 
-            <!--Fim do card de busca-->
+                <!--Início do card de Listagem de Marcas-->
 
-            <!--Início do card de Listagem de Marcas-->
+                <card-component titulo="Listagem de Marcas">
+                    <template v-slot:conteudo>
+                        <table-component></table-component>
+                    </template>
 
-            <card-component titulo="Listagem de Marcas">
-                <template v-slot:conteudo>
-                    <table-component></table-component>
-                </template>
+                    <template v-slot:rodape>
+                        <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#modalMarca">Adicionar</button>
+                    </template>
+                </card-component>
 
-                <template v-slot:rodape>
-                     <button type="button" class="btn btn-primary btn-sm ">Adicionar</button>
-                </template>
-            </card-component>
+                <!--Fim do card de Listagem de Marcas-->
 
-            <!--Fim do card de Listagem de Marcas-->
-
+            </div>
         </div>
+        <modal-component id="modalMarca" titulo="Adicionar Marca">
+
+            <template v-slot:conteudoModal>
+
+                <div class="form-group">
+                    <input-container-component
+                        titulo="Nome da marca"
+                        id="novoNome"
+                        id-help="novoNomeHelp"
+                        texto-help="Informe o Nome da Marca."
+                    >
+                        <input type="text" class="form-control" id="novoNome" aria-describedby="novoNomeHelp" placeholder="Nome da marca">
+                    </input-container-component>
+                </div>
+
+                <div class="form-group">
+                    <input-container-component
+                        titulo="Imagem"
+                        id="novoImagem"
+                        id-help="novoImagemHelp"
+                        texto-help="Selecione uma imagem no formato png."
+                    >
+                        <input type="file" class="form-control-file" id="novoImagem" aria-describedby="novoImagemHelp" placeholder="Selecione uma imagem.">
+                    </input-container-component>
+                </div>
+
+            </template>
+
+            <template v-slot:rodape>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"> Fechar </button>
+                <button type="button" class="btn btn-primary"> Salvar </button>
+            </template>
+        </modal-component>
     </div>
-</div>
 </template>
